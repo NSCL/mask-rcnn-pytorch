936e2ba2e455d275175843869ff6883beef66ee0
CzJaewan <wodhks7968@gmail.com>
2019-10-02 05:38:20 +0900

    Revise nobrand_detect_image.py

--- nobrand_detect_image.py
+++ nobrand_detect_image.py
@@ -13,57 +13,74 @@ import model as modellib
 import visualize
 
 import torch
+import argparse
 
 
-# Root directory of the project
-ROOT_DIR = os.getcwd()
 
-# Directory to save logs and trained model
-MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
 
-# Path to trained weights file
-# Download this file and place in the root of your
-# project (See README file for details)
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco_0160.pth")
+    parser.add_argument('--images', required=False,
+                        default="images",
+                        help='Directory of the images')
+    
+    parser.add_argument('--weights', required=False,
+                        default="mask_rcnn_coco_0160.pth",
+                        help="Path to weights .pth file or 'coco'")
 
-# Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+    args = parser.parse_args()
 
-class InferenceConfig(coco.CocoConfig):
-    # Set batch size to 1 since we'll be running inference on
-    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-    # GPU_COUNT = 0 for CPU
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-    NUM_CLASSES = 4
 
-config = InferenceConfig()
-config.display()
+    # Root directory of the project
+    ROOT_DIR = os.getcwd()
 
-# Create model object.
-model = modellib.MaskRCNN(model_dir=MODEL_DIR, config=config)
-if config.GPU_COUNT:
-    model = model.cuda()
+    # Directory to save logs and trained model
+    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
-# Load weights trained on MS-COCO
-model.load_state_dict(torch.load(COCO_MODEL_PATH))
+    # Path to trained weights file
+    # Download this file and place in the root of your
+    # project (See README file for details)
+    COCO_MODEL_PATH = os.path.join(ROOT_DIR, args.weights)
 
-# COCO Class names
-# Index of the class in the list is its ID. For example, to get ID of
-# the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG','wet_tissue','food_wrap','steel_scourer']
+    # Directory of images to run detection on
+    IMAGE_DIR = os.path.join(ROOT_DIR, args.images)
 
-# Load a random image from the images folder
-file_names = next(os.walk(IMAGE_DIR))[2]
-image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+    class InferenceConfig(coco.CocoConfig):
+        # Set batch size to 1 since we'll be running inference on
+        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+        # GPU_COUNT = 0 for CPU
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+        NUM_CLASSES = 4
 
-# Run detection
+    config = InferenceConfig()
+    config.display()
 
-results = model.detect([image])
+    # Create model object.
+    model = modellib.MaskRCNN(model_dir=MODEL_DIR, config=config)
+    if config.GPU_COUNT:
+        model = model.cuda()
 
-# Visualize results
-matplotlib.use('TkAgg')
-r = results[0]
-visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+    # Load weights trained on MS-COCO
+    model.load_state_dict(torch.load(COCO_MODEL_PATH))
+
+    # COCO Class names
+    # Index of the class in the list is its ID. For example, to get ID of
+    # the teddy bear class, use: class_names.index('teddy bear')
+    class_names = ['BG','wet_tissue','food_wrap','steel_scourer']
+
+    # Load a random image from the images folder
+    file_names = next(os.walk(IMAGE_DIR))[2]
+    image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+    # Run detection
+
+    results = model.detect([image])
+
+    # Visualize results
+    matplotlib.use('TkAgg')
+    r = results[0]
+    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
                            class_names, r['scores'])
-plt.show()
+    plt.show()
